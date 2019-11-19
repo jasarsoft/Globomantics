c7a4b79d15affe740c29cbed2ae8e3e30955e4c2
Edin Jašarević <edinjasar14@gmail.com>
2019-11-19 11:21:16 +0100

    structure project

--- Globomantics/Services/IConferenceService.cs
+++ Globomantics/Services/IConferenceService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,9 @@ namespace Globomantics.Services
 {
     public interface IConferenceService
     {
+        Task<IEnumerable<ConferenceModel>> GetAll();
+        Task<ConferenceModel> GetById(int id);
+        Task<StatisticsModel> GetStatistics();
+        Task Add(ConferenceModel model);
     }
 }
