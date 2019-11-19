d2409bb13301f101ceae0222dee3e1e51f27952e
Edin Jašarević <edinjasar14@gmail.com>
2019-11-19 12:49:49 +0100

    module 3

--- Globomantics/Controllers/ConferenceController.cs
+++ Globomantics/Controllers/ConferenceController.cs
@@ -1,5 +1,6 @@
 ﻿using Globomantics.Services;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +10,32 @@ namespace Globomantics.Controllers
 {
     public class ConferenceController : Controller
     {
-        public ConferenceController(IConferenceService conferenceService )
-        {
+        private readonly IConferenceService conferenceService;
 
+        public ConferenceController(IConferenceService conferenceService)
+        {
+            this.conferenceService = conferenceService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.Title = "Conferences";
+            return View(await conferenceService.GetAll());
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Title = "Add Conference";
+            return View(new ConferenceModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ConferenceModel model)
+        {
+            if (ModelState.IsValid)
+                await conferenceService.Add(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
--- Globomantics/Startup.cs
+++ Globomantics/Startup.cs
@@ -6,6 +6,7 @@ using Globomantics.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,12 +14,22 @@ namespace Globomantics
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
             services.AddSingleton<IConferenceService, ConferenceMemoryService>();
+            services.AddSingleton<IProposalService, ProposalMemoryService>();
+
+            services.Configure<GlobomanticsOptions>(configuration.GetSection("Globomantics"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,8 +48,8 @@ namespace Globomantics
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Conference}/{action=Index}/{id?}"); 
+                   name: "default",
+                   pattern: "{controller=Conference}/{action=Index}/{id?}");
             });
         }
     }
